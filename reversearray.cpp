d70c106870391a8b912bc7e4aa331690f5dc41c2
mfarah86 <47376986+mfarah86@users.noreply.github.com>
2019-02-05 21:49:59 -0800

    Add files via upload

---
+++ reversearray.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+//int reverseArray(int *array [4])
+//{
+//  for (int i = 4; i > 0; i++)
+//  {
+//    array [i] = i;
+//  }
+
+//  return array [];
+//}
+
+int main()
+{
+  int array [4] = {1,2,3,4};
+  cout << array << endl;
+
+  reverseArray(array);
+  cout << array << endl;
+
+  return 0;
+}
