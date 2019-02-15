3e5711af625ebe38402717201c3208477a6e7df7
mfarah86 <47376986+mfarah86@users.noreply.github.com>
2019-02-14 21:47:39 -0800

    Update c++ input.cpp

--- c++ input.cpp
+++ c++ input.cpp
@@ -1,26 +1,61 @@
 #include <iostream>
 #include <string>
+#include <cstring>
 #include <fstream>
 #include "person.h"
 
 using namespace std;
 
+int numLines();
+void getRecords(int numRecords);
+
 int main()
 {
+  int numRecords;
+
+  numRecords = numLines();
+
+  Person record[numRecords];
+
+  getRecords(numRecords);
+
+  return 0;
+}
+
+int numLines()
+{
+  string line;
+  int numRecords;
   ifstream inData;
-  int records = 0;
-  string rest = "";
   inData.open("data.txt");
 
-  while(inData)
+  while(!inData.eof())
   {
-    getline(inData, rest);
-    records++;
+    getline(inData, line);
+    cout << line << " " << endl;
+    numRecords++;
+
   }
+  cout << numRecords << endl;
+  inData.close();
 
+  return numRecords;
+}
 
+void getRecords(int numRecords)
+{
+  ifstream inData;
+  string first;
+  string last;
+  string full;
 
-
-  inData.close("data.txt");
-  return 0;
+  inData.open("data.txt");
+  for (int i=0; i < numRecords; i++)
+  {
+    inData >> first;
+    inData >> last;
+    full = first + " " + last;
+    strcpy(record[i].Name, full.c_str());
+  }
+  inData.close();
 }
