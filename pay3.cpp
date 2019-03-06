ee61d9485f5ed27d74e1c226ed49763819d25f23
mfarah86 <47376986+mfarah86@users.noreply.github.com>
2019-03-05 21:42:42 -0800

    Update pay3.cpp

--- pay3.cpp
+++ pay3.cpp
@@ -2,32 +2,29 @@
 #include <string>
 #include <fstream>
 #include <vector>
+#include <iomanip>
 #include "person3.h"
 #include "person3.cpp"
 
-int readData(vector<Person> &emp);
-int companyNames(int size, vector<Person> &emp, vector<string> &comp);
-void printHighestPaid(int size, vector<Person> &emp);
-void separateAndSave(int eSize, int cSize, vector<Person> &emp, vector<string> &cNames);
+void readData(vector<Person> &emp);
+void companyNames(vector<Person> &emp, vector<string> &comp);
+void printHighestPaid(vector<Person> &emp);
+void separateAndSave(vector<Person> &emp, vector<string> &cNames);
 
 int main()
 {
   vector<string> cNames;
   vector<Person> employees;
-  int fileSize;
-  int compSize;
-
-  fileSize = readData(employees);
-  compSize = companyNames(fileSize, employees, cNames);
-  printHighestPaid(fileSize, employees);
-  separateAndSave(fileSize, compSize, employees, cNames);
+  readData(employees);
+  companyNames(employees, cNames);
+  printHighestPaid(employees);
+  separateAndSave(employees, cNames);
 
   return 0;
 }
 
-int readData(vector<Person> &emp)
+void readData(vector<Person> &emp)
 {
-  int i;
   string firstName;
   string lastName;
   int idNum;
@@ -37,60 +34,54 @@ int readData(vector<Person> &emp)
   ifstream inFile;
   inFile.open("input3.txt");
   Person p;
-
   inFile >> firstName;
   while(!inFile.eof())
   {
     inFile >> lastName >> idNum >> compName >> hours >> rate;
-
     p.setFirstName(firstName);
     p.setLastName(lastName);
     p.setEmployeeId(idNum);
     p.setCompanyName(compName);
     p.setHoursWorked(hours);
     p.setPayRate(rate);
-
     inFile >> firstName;
-
     emp.push_back(p);
-
-    //cout << emp.at(i).getFirstName() << " " << emp.at(i).getLastName() << " " << emp.at(i).getEmployeeId() << " "
-    //<< emp.at(i).getCompanyName() << " " << emp.at(i).getHoursWorked() << " " << emp.at(i).getPayRate() << endl;
-
-    i++;
   }
-  cout << endl;
   inFile.close();
-
-  return i;
 }
 
-int companyNames(int size, vector<Person> &emp, vector<string> &comp)
+void companyNames(vector<Person> &emp, vector<string> &comp)
 {
-  int count = 1;
+  int count = 0;
+  bool duplicate = false;
   string temp1 = "";
   string temp2 = "";
-  for (int i = 0; i < size; i++)
+  comp.push_back(emp.at(0).getCompanyName());
+  for(int i = 0; i < emp.size(); i++)
   {
+    duplicate = false;
     temp1 = emp.at(i).getCompanyName();
-    for (int j = 0; j < count; j++)
+    for(int j=0; j < comp.size(); j++)
     {
       temp2 = comp.at(j);
-      if (temp1==temp2)
+      if(temp1==temp2)
       {
-        count++;
+        duplicate = true;
+        break;
       }
     }
+    if(!duplicate)
+    {
+      comp.push_back(temp1);
+    }
   }
-
-  return count;
 }
 
-void printHighestPaid(int size, vector<Person> &emp)
+void printHighestPaid(vector<Person> &emp)
 {
   float highestPay = 0;
   int empLoc = 0;
-  for(int i = 0; i < size; i++)
+  for(int i = 0; i < emp.size(); i++)
   {
     if(emp.at(i).totalPay() > highestPay)
     {
@@ -98,27 +89,33 @@ void printHighestPaid(int size, vector<Person> &emp)
       empLoc = i;
     }
   }
-
-  cout << "Highest paid: " << emp.at(empLoc).fullName() << endl
-  << "Employee ID: " << emp.at(empLoc).getEmployeeId() << endl
-  << "Employer: " << emp.at(empLoc).getCompanyName() << endl
-  << "Total Pay: $" << emp.at(empLoc).totalPay() << endl;
-
+  cout.precision(2);
+  cout << setw(15) << left << "Highest paid: " << emp.at(empLoc).fullName() << endl
+  << setw(15) << left << "Employee ID: " << emp.at(empLoc).getEmployeeId() << endl
+  << setw(15) << left << "Employer: " << emp.at(empLoc).getCompanyName() << endl
+  << setw(15) << left << "Total Pay: " << "$" << fixed << emp.at(empLoc).totalPay() << endl;
 }
 
-void separateAndSave(int eSize, int cSize, vector<Person> &emp, vector<string> &cNames)
+void separateAndSave(vector<Person> &emp, vector<string> &cNames)
 {
   ofstream outFile;
   string temp1;
   string temp2;
-  for (int i = 0; i < eSize; i++)
+  for (int i = 0; i < emp.size(); i++)
   {
     temp1 = emp.at(i).getCompanyName();
-    for (int j = 0; j < cSize; j++)
+    for (int j = 0; j < cNames.size(); j++)
     {
-
+      temp2 = cNames.at(j);
+      if(temp1==temp2)
+      {
+        outFile.open(temp1+".txt", outFile.out | outFile.app);
+        outFile.precision(2);
+        outFile << " " << setw(20) << left << emp.at(i).fullName() << " " << setw(5) << left << emp.at(i).getEmployeeId()
+        << " " << setw(10) << left << emp.at(i).getCompanyName() << " " << setw(15) << left << fixed << emp.at(i).totalPay() << endl;
+        outFile.close();
+        break;
+      }
     }
-
   }
-
 }
